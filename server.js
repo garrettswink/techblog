6ab99fe6d1a3388ab53f58408f75c597b81d0abd
Garrett <garrettswink421@gmail.com>
2023-09-10 17:47:18 -0400

    Server, Sequelize, Connection, Schema

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+}
+);
