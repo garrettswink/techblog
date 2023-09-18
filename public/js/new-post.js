e600d4cfe9f701ea0fcf5892424aa039e419408c
Garrett <garrettswink421@gmail.com>
2023-09-18 11:55:50 -0400

    Handlebar + Front JS Updates

---
+++ public/js/new-post.js
@@ -0,0 +1,27 @@
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-new-post').value.trim();
+    const content = document.querySelector('#content-new-post').value.trim;
+
+    if (title && content) {
+        const response = await fetch ('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Post creation failed.');
+        }
+
+    }
+}
+
+
+const newPostForm = document.querySelector('.new-post-form');
+if (newPostForm) {
+    newPostForm.addEventListener('submit', newPostFormHandler);
+}
